6ddca13ce8cc4a7b91e69b1d33739dffa9f82418
John Lennon de Melo Fernandes <john.fernandes@keepsimple.com.br>
2017-11-17 13:40:01 -0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ControlsComponent } from './controls/controls.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlsComponent,
+    LeftPanelComponent
   ],
   imports: [
     BrowserModule
