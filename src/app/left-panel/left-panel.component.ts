678feb5eddd43c280f957d980a0c36d4b69c5693
John Lennon de Melo Fernandes <john.fernandes@keepsimple.com.br>
2017-11-17 13:50:31 -0200

    Mudando listagem de musicas

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-left-panel',
-  templateUrl: './left-panel.component.html',
-  styleUrls: ['./left-panel.component.scss']
+    selector: 'app-left-panel',
+    templateUrl: './left-panel.component.html',
+    styleUrls: ['./left-panel.component.scss']
 })
 export class LeftPanelComponent implements OnInit {
 
-  constructor() { }
+    public musics: any = [];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.musics = [
+            {
+                'artist': 'The beatles',
+                'music': 'helter skelter',
+                'duration': '3:12 m'
+            },
+            {
+                'artist': 'Metallica',
+                'music': 'the unforgiven',
+                'duration': '6:12m'
+            },
+            {
+                'artist': 'Judas Priest',
+                'music': 'Angel',
+                'duration': '4:30m'
+            },
+        ];
+    }
 
 }
