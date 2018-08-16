8bf6cc6a47a4c2f4271701fdf752e33256f69774
JOHN LENNON DE MELO FERNANDES <john.lennon@viavarejo.com.br>
2018-08-16 16:09:39 -0300

    corrigindo chmod

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -18,37 +18,7 @@ export class LeftPanelComponent implements OnInit {
                 'artist': 'The beatles',
                 'music': 'Helter Skelter',
                 'duration': '3:12 m'
-            },
-            {
-                'artist': 'Metallica',
-                'music': 'The Unforgiven',
-                'duration': '6:12m'
-            },
-            {
-                'artist': 'Judas Priest',
-                'music': 'Angel',
-                'duration': '4:30m'
-            },
-            {
-                'artist': 'Megadeth',
-                'music': 'Trust',
-                'duration': '5:30m'
-            },
-            {
-                'artist': 'Megadeth',
-                'music': 'Tornado of Souls',
-                'duration': '6:30m'
-            },
-            {
-                'artist': 'A Perfect Circle',
-                'music': 'Passive',
-                'duration': '4:30m'
-            },
-            {
-                'artist': 'Pantera',
-                'music': 'Cowboys From Hell',
-                'duration': '4:30m'
-            },
+            }
         ];
     }
 
